2d1b079128b42dc863907a6873a1c8affa40ffed
Slav Donchev <slav.donchev@gmail.com>
2020-12-24 13:39:27 +0200

    add previous modile code

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "s3_bucket_name" {
+  description = "The name of the bucket."
+  value       = aws_s3_bucket.tfe_s3_bucket.id
+}
+
+output "s3_bucket_region" {
+  description = "The AWS region the bucket resides in."
+  value       = aws_s3_bucket.tfe_s3_bucket.region
+}
+
+output "pg_address" {
+  description = "The hostname of the PostgreSQL instance."
+  value       = aws_db_instance.tfe.address
+}
